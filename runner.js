6020b079a2a73bbcbd86bdee08855764c1f7139d
Connor Bush <connormbush12@gmail.com>
2020-11-20 18:18:56 -0600

    Collect all test files

--- runner.js
+++ runner.js
@@ -1,17 +1,35 @@
-//We want to use File Systems' file and directory reading functions for our collectFiles function
 const fs = require('fs')
+//Gotta require in path to use path.join
+const path = require('path')
 
 class Runner {
     constructor() {
-        this.files = [];
+        this.testFiles = [];
     }
-    //We make collectFiles async so that we can use the promised-based version of fs.readdir
-    //We also pass through our targetPath
     async collectFiles(targetPath) {
-        //We make our files variable equal to the readdir() function and then return the files
-        const files = fs.promises.readdir(targetPath)
+        const files = await fs.promises.readdir(targetPath)
+        //Now that we have an array of all of the files / folders we have, we want to find any test files by looking through all of the folders.
+        for (let file of files) {
+            //First, we get the full filepath for the file by joining the targetpath and the file name
+            const filepath = path.join(targetPath, file)
+            
+            //Now, we want to get the stats about that file to determine if it's a file or directory
+            const stats = await fs.promises.lstat(filepath);
 
-        return files;
+            //First, we check for two things: if it is a file AND if so, if the file includes .test.js
+            if(stats.isFile() && file.includes('.test.js')) {
+                //If it is a test file, then we want to push it as an object itno our testFiles array so we can get that information later
+                this.testFiles.push({name : filepath})
+            } else if(stats.isDirectory()) {
+                //If we have another directory, we essentially want to do what we did above: read the files in it and save it all to an array.
+                const childFiles = await fs.promises.readdir(filepath)
+
+                //Now, we push each element of this childFiles array into files by spreading it in there.
+                //We also have to make sure we capture the entire path name, so we don't push in childFiles, rather we map over it and create an array with their full paths shown and then spread that into files
+                files.push(...childFiles.map(f => path.join(file, f)));
+            }
+
+        }
     }
 }
 
