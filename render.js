2d61dc740ca516f39cddfc97bb9aa3df6a0e9bb1
Connor Bush <connormbush12@gmail.com>
2020-11-23 10:34:20 -0600

    Tests for valid and invalid email + script delay

--- render.js
+++ render.js
@@ -1,21 +1,19 @@
-//We create a render function that we can use to render any JavaScript web-based programs for testing purposes
-
 const path = require('path')
 const jsdom = require('jsdom')
 const {JSDOM} = jsdom
 
 const render = async (filename) => {
-    //The file path is the current working directory joined with the individual filename
     const filePath = path.join(process.cwd(), filename)
-
-    //We use the JSDOM.fromFile() functoin to create our dom using the file path
-    const dom = JSDOM.fromFile(filePath, {
+    const dom = await JSDOM.fromFile(filePath, {
         runScripts : 'dangerously',
         resources : 'usable'
     })
 
-    return dom;
-}
+    return new Promise((resolve, reject) => {
+        dom.window.document.addEventListener('DOMContentLoaded', () => {
+            resolve(dom)
+        })
+    })
+  }
 
-//We export out the function itself, not an execution of the function (which would be module.exports = render();)
 module.exports = render;
--- sampleWebProject/test/app.test.js
+++ sampleWebProject/test/app.test.js
@@ -1,6 +1,7 @@
 const { italic } = require("chalk");
 
-const assert = require('assert')
+const assert = require('assert');
+const render = require("../../render");
 
 //As our first test, we see if the index.html file as an input text box
 it('has a text input', async () => {
@@ -10,4 +11,30 @@ it('has a text input', async () => {
 
     //If there is no input element, than the input variable will be undefined so the assert will send an error
     assert(input);
+})
+
+it('shows a success message with valid email', async() => {
+    const dom = await render('index.html')
+    const input = dom.window.document.querySelector('input');
+    input.value = 'coolfakeemail@coolcon.com'
+
+    dom.window.document.querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'))
+
+    const h1 = dom.window.document.querySelector('h1')
+
+    assert.strictEqual(h1.innerHTML, 'Looks good!')
+})
+
+it('shows a fail message with an invalid email', async() => {
+    const dom = await render('index.html')
+    const input = dom.window.document.querySelector('input');
+    input.value = 'badfakeemail'
+
+    dom.window.document.querySelector('form')
+        .dispatchEvent(new dom.window.Event('submit'))
+
+    const h1 = dom.window.document.querySelector('h1')
+
+    assert.strictEqual(h1.innerHTML, 'Invalid email')
 })
