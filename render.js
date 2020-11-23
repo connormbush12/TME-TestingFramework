37554cdd5be269b62c825d627921e9ec27582808
Connor Bush <connormbush12@gmail.com>
2020-11-23 09:59:30 -0600

    Render function for JSDOM

---
+++ render.js
@@ -0,0 +1,21 @@
+//We create a render function that we can use to render any JavaScript web-based programs for testing purposes
+
+const path = require('path')
+const jsdom = require('jsdom')
+const {JSDOM} = jsdom
+
+const render = (filename) => {
+    //The file path is the current working directory joined with the individual filename
+    const filePath = path.join(process.cwd(), filename)
+
+    //We use the JSDOM.fromFile() functoin to create our dom using the file path
+    const dom = JSDOM.fromFile(filePath, {
+        runScripts : 'dangerously',
+        resources : 'usable'
+    })
+
+    return dom;
+}
+
+//We export out the function itself, not an execution of the function (which would be module.exports = render();)
+module.exports = render;
--- runner.js
+++ runner.js
@@ -2,7 +2,9 @@ const fs = require('fs')
 const path = require('path')
 const chalk = require('chalk')
 
-//We create an array of forbidden directories that we don't want our testing framework to check. Typically, this will include node-modules
+//To make the render funciton available anywhere in our project, we require it in here and set it as a property on the global object.
+const render = require('./render')
+
 const forbiddenDirs = ['node_modules']
 
 class Runner {
@@ -14,6 +16,7 @@ class Runner {
         for (let file of this.testFiles) {
             console.log(chalk.gray(`--- ${file.shortName}`))
             const beforeEaches = [];
+            global.render = render;
             global.beforeEach = (fn) => {
                 beforeEaches.push(fn)
             }
@@ -29,7 +32,6 @@ class Runner {
                     console.log(chalk.red('\t', message))
                 }
             }
-            //We could also have an error requiring in our file as well
             try {
                 require(file.name)
             } catch(err) {
@@ -45,7 +47,6 @@ class Runner {
             const stats = await fs.promises.lstat(filepath);
             if(stats.isFile() && file.includes('.test.js')) {
                 this.testFiles.push({name : filepath, shortName : file})
-                //Not only must the file be a directory for us to look into it, now it much not be in our list of forbidden directories
             } else if(stats.isDirectory() && !forbiddenDirs.includes(file)) {
                 const childFiles = await fs.promises.readdir(filepath)
 
